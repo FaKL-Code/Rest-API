06b5a05ddc99937fa173e4c9cf6f0aea78523b14
Julio Facal <starlyn.dev@gmail.com>
2022-10-28 09:29:26 -0300

    criando uma validacao incial

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,3 +1,4 @@
+from xml.dom import ValidationErr
 from rest_framework import serializers
 from clientes.models import Cliente
 
@@ -5,4 +6,24 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+    
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("CPF invalido")
+        return cpf
+    
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Nome não aceita caracteres numéricos")
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("RG invalido")
+        return rg
+    
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("Celular invalido")
+        return celular
     
